7615672cff37f9e98f1e566c05b03ad6ae5dbe1b
Marc Teitelbaum <marcteit@gmail.com>
2017-10-08 00:40:14 -0300

    Build task

--- entry_point.py
+++ entry_point.py
@@ -5,6 +5,3 @@ def lambda1():
     _sample_data_access = sample_data_access()
     _sample_business_logic = sample_business_logic(_sample_data_access)
     print(_sample_business_logic.execute())
-
-def lambda2():
-    print("foo")
--- tasks.py
+++ tasks.py
@@ -1,8 +1,20 @@
 from invoke import task
+from invoke import run
 import entry_point
 import os
+import zipfile
 
 @task
 def lambda1(ctx):
     os.environ['a'] = 'b'
     entry_point.lambda1()
+
+@task
+def build(ctx):
+    run('pip install -r requirements.txt -t .', hide=True)
+    build_file = zipfile.ZipFile('build.zip', 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk('.'):
+        if root.find('.git') == -1:
+            for file in files:
+                build_file.write(os.path.join(root, file))
+    build_file.close()
